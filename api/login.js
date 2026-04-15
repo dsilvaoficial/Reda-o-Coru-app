30cfd4e49b0024c2cd4a20a1824de7880f34a103
D.Silva <deusirenee@gmail.com>
2026-04-15 15:25:40 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,10 +1,21 @@
-export default async function handler(req, res) {
-  const { email, senha } = req.body
-
-  // teste simples
-  if(email === "admin@test.com" && senha === "123"){
-    res.status(200).json({ sucesso: true })
-  } else {
-    res.status(200).json({ sucesso: false })
+let usuarios = {
+  "teste@teste.com": {
+    senha: "123",
+    creditos: 10
   }
+};
+
+export default function handler(req, res) {
+  const { email, senha } = req.body;
+
+  const user = usuarios[email];
+
+  if (!user || user.senha !== senha) {
+    return res.status(401).json({ erro: "Login inválido" });
+  }
+
+  res.status(200).json({
+    mensagem: "Login ok",
+    email: email
+  });
 }
