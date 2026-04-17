95044130df9235cf8e08121fc102a8f2e617c33e
D.Silva <deusirenee@gmail.com>
2026-04-17 15:14:05 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const SECRET = "segredo_super_secreto"; // depois vamos melhorar isso
+const SECRET = process.env.JWT_SECRET;
 
 let usuarios = {
     "teste@teste.com": {
